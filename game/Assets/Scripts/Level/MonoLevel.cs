2a9ac6001b6a6093dac7f7cc73165d887665eaa5
Quinn <99677023+thepigeongenerator@users.noreply.github.com>
2024-10-05 15:33:55 +0200

    use monotiles now

--- game/Assets/Scripts/Level/MonoLevel.cs
+++ game/Assets/Scripts/Level/MonoLevel.cs
@@ -1,13 +1,12 @@
-using System.ComponentModel;
 using UnityEngine;
 
 public class MonoLevel : MonoBehaviour
 {
     [SerializeField, Min(1)] private int width = 10;    // non-negative value corresponding to the maximum amount of tiles on the horizontal axis
     [SerializeField, Min(1)] private int height = 10;   // non-negative value corresponding to the maximum amount of tiles on the vertucal axis
-    [SerializeField] private GameObject pathPrefab;     // used to instantiate the path
+    [SerializeField] private MonoTile pathPrefab;       // contains the prefab which contains a singular node of the path
 
-    private Level level = null;
+    public Level level = null;
 
     private void Awake()
     {
@@ -16,7 +15,9 @@ public class MonoLevel : MonoBehaviour
 
         foreach (Vector2Int pathPos in level.GetPath())
         {
-            Instantiate(pathPrefab, new Vector3(pathPos.x + 0.5F, 0, pathPos.y + 0.5F), Quaternion.identity, transform);
+            MonoTile tile = Instantiate(pathPrefab, new Vector3(pathPos.x + 0.5F, 0, pathPos.y + 0.5F), Quaternion.identity, transform);
+            tile.SetLevel(level);
+            tile.SetTilePos(pathPos);
         }
     }
 
---
+++ game/Assets/Scripts/Level/MonoTile.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+public class MonoTile : MonoBehaviour
+{
+    private Level level = null;
+    private Vector2Int tilePos = Vector2Int.one * -1;
+
+    public TileData Data => level.GetTile(tilePos);
+
+    /// <summary>
+    /// sets the level, throws <see cref="InvalidOperationException"/> when already set
+    /// </summary>
+    public void SetLevel(Level level)
+    {
+        if (level != null) throw new InvalidOperationException("attempted to set level whilst level was already set!");
+        this.level = level;
+    }
+
+    /// <summary>
+    /// sets the tile position, throws <see cref="InvalidOperationException"/> when already set
+    /// </summary>
+    public void SetTilePos(Vector2Int tilePos)
+    {
+        if (tilePos != (Vector2Int.one * -1)) throw new InvalidOperationException("attempted to set the tile position whilst the tile position was already set");
+        this.tilePos = tilePos;
+    }
+}
