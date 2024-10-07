65cc25d158016a8e8676d825b8ff15f524bcab4d
Lyritha <Collin.rarekip@gmail.com>
2024-10-07 16:29:54 +0200

    started with touch detection for grid placement

--- game/Assets/Scripts/PlaceOnGrid.cs
+++ game/Assets/Scripts/PlaceOnGrid.cs
@@ -4,6 +4,9 @@ public class PlaceOnGrid : MonoBehaviour
 {
     [SerializeField] private GameObject objectToPlace;
 
+    [SerializeField] private Vector3 screenPosition;
+    [SerializeField] private Vector3 worldPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,18 @@ public class PlaceOnGrid : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            //if you touch screen
+            if (touch.phase == TouchPhase.Began)
+            {
+                Vector3 worldPos = Camera.main.ScreenToWorldPoint(new(touch.position.x, touch.position.y, 0));
+            }
+        }
+
+
     }
+
 }
