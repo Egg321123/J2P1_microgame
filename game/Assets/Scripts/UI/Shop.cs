d49bea663f749d0f2946e1fc158f1400bb6ad83c
Daniël Kosters <0356076@student.rocvantwente.nl>
2024-10-10 13:36:21 +0200

    update shop

--- game/Assets/Scripts/UI/Shop.cs
+++ game/Assets/Scripts/UI/Shop.cs
@@ -7,65 +7,43 @@ public class Shop : MonoBehaviour
 {
     string[] towers = new string[4];
     [SerializeField] List<TowerStoreData> itemData = new List<TowerStoreData>();
-    [SerializeField] List<Sprite> images = new List<Sprite>();
     [SerializeField] List<GameObject> toggles = new List<GameObject>();
-    [SerializeField] Money_System money;
-    //[SerializeField] List<TextMeshPro> itemInfo;
-    // Start is called before the first frame update
+    private PlaceOnGrid place;
+
     void Start()
     {
-       LoadShop();
-        money = GetComponentInParent<Money_System>();
+        LoadShop();
+        place = FindFirstObjectByType<PlaceOnGrid>();
     }
-    void LoadShop()
+    public void LoadShop()
     {
         List<TowerStoreData> tItemData = new List<TowerStoreData>();
-        List<Sprite> tImages = new List<Sprite>();
         foreach (var data in itemData) tItemData.Add(data);
-        foreach (var sprite in images) tImages.Add(sprite);
 
         for (int toggle = 0; toggle < toggles.Count; toggle++)
         {
             Transform gOText = toggles[toggle].transform.GetChild(0);
-            // itemInfo = new List<TextMeshPro>(buttons[button].GetComponentInChildren<TextMeshPro>());
-            //List<> itemData = new List<>();
             int randomItem = Random.Range(0, tItemData.Count);
             for (int text = 0; text < gOText.childCount; text++)
             {
                 string data;
-                if (text == 0) data = tItemData[randomItem].name;
+                if (text == 0) data = tItemData[randomItem].towerName;
                 else data = "Price: " + tItemData[randomItem].cost.ToString();
                 gOText.GetChild(text).GetComponent<TextMeshProUGUI>().text = data;
             }
-            towers[toggle] = tItemData[randomItem].name;
-            toggles[toggle].transform.GetChild(1).GetComponent<Image>().sprite = tImages[randomItem];
-            tImages.RemoveAt(randomItem);
+            towers[toggle] = tItemData[randomItem].towerName;
+            toggles[toggle].transform.GetChild(1).GetComponent<Image>().sprite = tItemData[randomItem].menuImg;
+            //tImages.RemoveAt(randomItem);
             tItemData.RemoveAt(randomItem);
         }
     }
     TowerStoreData FindTower(string tower2Search)
     {
-        for ( int tower = 0; tower < itemData.Count; tower++) if (itemData[tower].name == tower2Search) return itemData[tower];
+        for (int tower = 0; tower < itemData.Count; tower++) if (itemData[tower].towerName == tower2Search) return itemData[tower];
         return null;
     }
-    public void item1()
-    {
-        TowerStoreData tower = FindTower(towers[0]);
-        if (money.Pay(tower.cost)) FindFirstObjectByType<PlaceOnGrid>().PlaceModeToggle(tower);
-    }
-    public void item2()
-    {
-        FindFirstObjectByType<PlaceOnGrid>().PlaceModeToggle(FindTower(towers[1]));
-
-    }
-    public void item3()
-    {
-        FindFirstObjectByType<PlaceOnGrid>().PlaceModeToggle(FindTower(towers[2]));
-
-    }
-    public void item4()
-    {
-        FindFirstObjectByType<PlaceOnGrid>().PlaceModeToggle(FindTower(towers[3]));
-
-    }
+    public void item1() => place.PlaceModeToggle(FindTower(towers[0]));
+    public void item2() => place.PlaceModeToggle(FindTower(towers[1]));
+    public void item3() => place.PlaceModeToggle(FindTower(towers[2]));
+    public void item4() => place.PlaceModeToggle(FindTower(towers[3]));
 }
