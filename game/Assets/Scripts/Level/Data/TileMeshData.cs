add9c270da2b9426cfb022bf64f28e275f00ffa1
Quinn <99677023+thepigeongenerator@users.noreply.github.com>
2024-10-14 15:13:45 +0200

    singular neighbour model processing

--- game/Assets/Scripts/Level/Data/TileMeshData.cs
+++ game/Assets/Scripts/Level/Data/TileMeshData.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 [Serializable]
 public struct TileMeshData
 {
-    public Mesh straight;     // has 2 opposing neighbours
-    public Mesh corner;       // has 2 adjointed neighbours
-    public Mesh single;       // has 3 adjointed neighbours
-    public Mesh full;         // has no neighbours
-    public Mesh empty;        // has neighbours on each side
+    public Mesh end;            // has 1 adjointed neighbour
+    public Mesh straight;       // has 2 opposing neighbours
+    public Mesh corner;         // has 2 adjointed neighbours
+    public Mesh single;         // has 3 adjointed neighbours
+    public Mesh full;           // has no neighbours
+    public Mesh empty;          // has neighbours on each side
 }
