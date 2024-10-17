d8f8f4170c94f2478a236732091a98e73440a9e1
Lyritha <Collin.rarekip@gmail.com>
2024-10-17 11:21:26 +0200

    Improved audio toggles to be remembered

--- game/Assets/Scripts/Utility/AudioToggle.cs
+++ game/Assets/Scripts/Utility/AudioToggle.cs
@@ -1,22 +1,40 @@
+using System;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioToggle : MonoBehaviour
 {
+    [SerializeField] Toggle FXToggle;
+    [SerializeField] Toggle MusicToggle;
+
     [SerializeField] AudioMixer audioMixer;
-    bool isAudioOn = true;
+
+    private void Start()
+    {
+        //get the toggles from playerprefs
+        FXToggle.isOn = PlayerPrefs.GetInt("FXToggle", 1) == 1;
+        MusicToggle.isOn = PlayerPrefs.GetInt("MusicToggle", 1) == 1;
+
+        ToggleFX();
+        ToggleMusic();
+    }
 
     public void ToggleFX()
     {
-        float volume = isAudioOn ? -80 : 0;
+        bool isOn = FXToggle.isOn;
+        float volume = isOn ? 0 : -80;
+
         audioMixer.SetFloat("FXVol", volume);
-        isAudioOn = !isAudioOn;
+        PlayerPrefs.SetInt("FXToggle", Convert.ToInt32(isOn));
     }
 
     public void ToggleMusic()
     {
-        float volume = isAudioOn ? -80 : 0;
+        bool isOn = MusicToggle.isOn;
+        float volume = isOn ? 0 : -80;
+
         audioMixer.SetFloat("MusicVol", volume);
-        isAudioOn = !isAudioOn;
+        PlayerPrefs.SetInt("MusicToggle", Convert.ToInt32(isOn));
     }
 }
