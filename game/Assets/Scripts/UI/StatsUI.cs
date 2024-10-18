59859808cc0158f68fc3d31f0f95e0fd44fcd4eb
Daniël Kosters <0356076@student.rocvantwente.nl>
2024-10-18 16:33:10 +0200

    Update StatsUI.cs

--- game/Assets/Scripts/UI/StatsUI.cs
+++ game/Assets/Scripts/UI/StatsUI.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class StatsUI : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI[] statUIs;
+    [SerializeField] TextMeshProUGUI statUI;
     private void Start()
     {
         UpdateStats();
@@ -17,29 +17,16 @@ public class StatsUI : MonoBehaviour
     {
         Statistics stats = GameManager.Instance.Save.data.stats;
 
-        foreach (TextMeshProUGUI ui in statUIs)
-        {
-            ui.text = 
-                $"\nKills: {stats.kills}\n " +
-                $"Towers Placed: {stats.towersPlaced}\n" +
-                $"Towers Upgraded: {stats.towersUpgraded}\n" +
-                $"Money Spend: {stats.moneySpent}\n\n" +
+        statUI.text =
+            $"\nKills: {stats.kills}\n " +
+            $"Towers Placed: {stats.towersPlaced}\n" +
+            $"Towers Upgraded: {stats.towersUpgraded}\n" +
+            $"Money Spend: {stats.moneySpent}\n\n" +
 
-                $"Total Kills: {stats.totalKills}\n " +
-                $"Total Towers Placed: {stats.totalTowersPlaced}\n" +
-                $"Total Towers Upgraded: {stats.totalTowersUpgraded}\n" +
-                $"Total Money Spend: {stats.totalMoneySpent}" ;
+            $"Total Kills: {stats.totalKills}\n " +
+            $"Total Towers Placed: {stats.totalTowersPlaced}\n" +
+            $"Total Towers Upgraded: {stats.totalTowersUpgraded}\n" +
+            $"Total Money Spend: {stats.totalMoneySpent}";
 
-           /* ui.text = string.Format(ui.text,
-                    stats.kills,                // {0}
-                    stats.towersPlaced,         // {1}
-                    stats.towersUpgraded,       // {2}
-                    stats.moneySpent,           // {3}
-                    stats.totalKills,           // {4}
-                    stats.totalTowersPlaced,    // {5}
-                    stats.totalTowersUpgraded,  // {6}
-                    stats.totalMoneySpent       // {7}
-            );*/
-        }
     }
 }
