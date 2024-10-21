1c87cd08cbf96dbe24a533f17425e20167ee6f7b
Lyritha <Collin.rarekip@gmail.com>
2024-10-21 13:08:57 +0200

    tweaked sound, fixed issue with clicking through ui

--- game/Assets/Scripts/PlaceOnGrid.cs
+++ game/Assets/Scripts/PlaceOnGrid.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlaceOnGrid : MonoBehaviour
 {
@@ -59,18 +60,21 @@ public class PlaceOnGrid : MonoBehaviour
                 Touch touch = Input.GetTouch(0);
 
                 //avoids race condition from ui toggle (takes longer to toggle then to send raycast)
-                yield return new WaitForSeconds(0.1f);
                 if (!isInPlaceMode) yield break;
 
                 //when it registers the touch from the screen
                 if (touch.phase == TouchPhase.Began)
                 {
+                    //makes sure you can't click through the ui
+                    if (EventSystem.current.IsPointerOverGameObject(0)) yield break;
+
                     //creates the ray with proper parameters
                     Ray ray = Camera.main.ScreenPointToRay(new(touch.position.x, touch.position.y, 0));
 
                     // Does the raycast check
                     if (Physics.Raycast(ray, out RaycastHit hit, 1000f, targetLayer))
                     {
+
                         // Gets the world position
                         Vector2Int pos = new((int)Mathf.Floor(hit.point.x), (int)Mathf.Floor(hit.point.z));
 
