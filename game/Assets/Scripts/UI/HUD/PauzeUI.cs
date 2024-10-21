8fdfe3a3b8767dd8778efe5795551c128ffe28ac
Quinn <99677023+thepigeongenerator@users.noreply.github.com>
2024-10-21 18:34:32 +0200

    comment out pause on lost focus due to issues it was causing

--- game/Assets/Scripts/UI/HUD/PauzeUI.cs
+++ game/Assets/Scripts/UI/HUD/PauzeUI.cs
@@ -6,7 +6,8 @@ public class PauzeUI : MonoBehaviour
 
     [SerializeField] GameObject gameUI;
     [SerializeField] GameObject pauseUI;
-    public void Pause(bool goalIsActive)//this method activate and deactivate the pause menue based on the parameter
+
+    public void Pause(bool goalIsActive) // activates and deactivates the pause menu based on the parameter
     {
         //if the goal is to activate the pause menu than freeze the game, otherwise the game will play as normal
         if (goalIsActive) {
@@ -17,8 +18,13 @@ public class PauzeUI : MonoBehaviour
         gameUI.SetActive(!goalIsActive);
         pauseUI.SetActive(goalIsActive);
     }
-    private void OnApplicationFocus(bool focus)//if the aplication loses the foces it will automaticly acitavte the pause menu
+
+    // this caused issues with the speed up button and other timings when another UI was visible, so disabled
+    // furthermore, it actually was quite annoying.
+    /*
+    private void OnApplicationFocus(bool focus) //if the application loses the focus it will automatically activate the pause menu
     {
         if (!focus) Pause(true);
     }
+    */
 }
